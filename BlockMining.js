680e3433b49dbfe5373cdade0f43558af28ebf4d
Tran Dang Cong <34366156+tdcong@users.noreply.github.com>
2025-04-07 10:04:46 +0700

    Create BlockMining.js

---
+++ BlockMining.js
@@ -0,0 +1,21 @@
+const SHA256 = require ('crypto-js/sha256');
+class Block{
+    constructor(index, timestamp, data, previousHash = ''   ){
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.computehash();
+    this.nonce = 0;
+    }
+    computehash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)+ this.nonce).toString();
+    }
+    Blockmining(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.computehash();
+        }
+        console.log("Block mined: " + this.hash);
+    }
+}
